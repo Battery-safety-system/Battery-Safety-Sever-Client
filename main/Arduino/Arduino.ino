09c8f342ab862c23c46eafb35214ecc85461acc4
blendid3 <zhqblendid@gmail.com>
2021-04-06 09:35:40 -0700

    arduino

--- main/Arduino/Arduino.ino
+++ main/Arduino/Arduino.ino
@@ -39,32 +39,32 @@ void loop() {
   String mapStatus = "Ardu_Temp1:" + String(Temp1) + ",Ardu_Temp2:" + String(Temp2) + ",Ardu_Press:" + String(Press) ;
   Serial.println(mapStatus);
   
-
- while(Serial.available() == 0) {
- }
-
-   char input[INPUT_SIZE + 1];
-   byte size = Serial.readBytes(input, INPUT_SIZE);
-   // Add the final 0 to end the C string
-   input[size] = 0;
-   char* command = strtok(input, "&");
-   while (command != 0)
-   {
-   // Split the command in two values
-       char* separator = strchr(command, ':');
-       if (separator != 0)
-       {
+  delay(500);
+  if(Serial.available() != 0) {
+    char input[INPUT_SIZE + 1];
+    byte size = Serial.readBytes(input, INPUT_SIZE);
+    // Add the final 0 to end the C string
+    input[size] = 0;
+    char* command = strtok(input, "&");
+    while (command != 0)
+    {
+    // Split the command in two values
+      char* separator = strchr(command, ':');
+      if (separator != 0)
+      {
        // Actually split the string in 2: replace ':' with 0
            *separator = 0;
            int servoId = atoi(command);
            ++separator;
            int val = atoi(separator);
            digitalWrite( servoId, val);
-
        // Do something with servoId and position
-        }
+      }
        // Find the next command in input string
-       command = strtok(0, "&");
+      command = strtok(0, "&");
    }
+ }
+
+
 
 }
